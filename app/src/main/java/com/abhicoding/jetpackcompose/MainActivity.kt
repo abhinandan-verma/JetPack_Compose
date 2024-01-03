bc8eafc691d4a5ae039a4783b29e1b9c53a74843
abhinandan-verma <abhinandanverma551@gmail.com>
2024-01-03 13:21:17 +0530

    TextField, Column , Row & Boxes

--- app/src/main/java/com/abhicoding/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/abhicoding/jetpackcompose/MainActivity.kt
@@ -1,42 +1,94 @@
 package com.abhicoding.jetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Text(text = "Hello Abhinandan")
+          TextInput()
         }
     }
 }
-@Preview(showBackground = true, widthDp = 500, heightDp = 300)
+@Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
-private fun PreviewFunction(){
-    Button(onClick = { }, colors = ButtonDefaults.buttonColors(
-        contentColor = Color.Magenta,
-        containerColor = Color.Green
-    ), enabled = true) {
-        Text(text = "Hello Abhinandan")
-        Image(painter = painterResource(id = R.drawable.photo),
-            contentDescription = "My Photo",
-            )
+private fun PreviewFunction() {
+    Column(
+        Modifier.padding(5.dp)
+    ) {
+        ListViewItem(R.drawable.photo,"Abhinandan","Software Engineer")
+        ListViewItem(R.drawable.profil,"Abhinandan","Software Engineer")
+        ListViewItem(R.drawable.trigo,"Abhinandan","Software Engineer")
+        ListViewItem(R.drawable.ic_launcher_foreground,"Abhinandan","Software Engineer")
     }
+
+
+  /*  Box(contentAlignment = Alignment.BottomEnd) {
+        Image(painter = painterResource(id = R.drawable.photo), contentDescription = "Photo")
+        Image(painter = painterResource(id = R.drawable.profil), contentDescription = "Profile")
+    }
+   */
+    /* Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "A", fontSize = 24.sp)
+        Text(text = "B", fontSize = 24.sp)
+    }
+    */
+/*
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "A", fontSize = 24.sp)
+        Text(text = "B", fontSize = 24.sp)
+    }
+ */
+}
+@Composable
+fun ListViewItem(imgId: Int, name: String,occupation:String){
+    Row {
+        Image(painter = painterResource(id = imgId),
+            contentDescription = "",
+            Modifier.size(100.dp))
+        Column {
+            Text(text = name,
+                fontWeight = FontWeight.Bold)
+            Text(text = occupation,
+                fontWeight = FontWeight.Thin,
+                fontSize = 12.sp)
+        }
+    }
+}
+@Composable
+fun TextInput(){
+    val state = remember {
+        mutableStateOf("AbhCoding")
+    }
+    TextField(value = state.value,
+        onValueChange = {
+            state.value = it
+            Log.i("TAG",it)
+        },
+        label = { Text(text = "Enter Name")})
 }
